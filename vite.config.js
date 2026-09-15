9af6b60c32b3e52ba6cbc0978cef5bf834457340
Ben McCarthy <bmccarthy90@gmail.com>
2026-09-15 11:59:27 -0400

    changed docs to dist in vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default {
     },
     build:
     {
-        outDir: '../dist', // Output in the dist/ folder
+        // changed for github pages
+        outDir: '../docs', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
         sourcemap: true, // Add sourcemap
         target: 'es2022'
